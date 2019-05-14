33d605ee7894313805aefdc30ea4debed88bd083
Robin Messenger <robin@6joes.com>
2019-05-14 15:35:55 -0300

    TypeScript not priority right now.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+const sanityClient = require('@sanity/client');
+
+Vue.config.productionTip = false
+Vue.prototype.$sanity = sanityClient({
+  projectId: 'wst1m5tr',
+  dataset: 'production'
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
